e2ec3ca5250410a04184d7b59c3721f47a9b3f85
Kushal3502 <kushal2002personal@gmail.com>
2024-05-11 15:24:11 +0530

    React Router

--- 07_reactRouter/src/App.jsx
+++ 07_reactRouter/src/App.jsx
@@ -1,12 +1,7 @@
 import "./App.css";
-import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <>
-      <Header/>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- 07_reactRouter/src/main.jsx
+++ 07_reactRouter/src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import Layout from "./routes/Layout.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import User from "./components/User/User.jsx";
+import Github, { githubInfo } from "./components/Github/Github.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        loader: githubInfo,
+        path: "github",
+        element: <Github />,
+      },
+      {
+        path: "user/:userId",
+        element: <User />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
