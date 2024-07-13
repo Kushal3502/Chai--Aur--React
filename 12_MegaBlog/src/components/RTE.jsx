ddc83b29c83b0aa31d5ffa1afdce40218d6d86b4
Kushal3502 <kushal2002personal@gmail.com>
2024-07-13 17:13:00 +0530

    Blog App

--- 12_MegaBlog/src/components/RTE.jsx
+++ 12_MegaBlog/src/components/RTE.jsx
@@ -7,7 +7,6 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
-
       <Controller
         name={name || "content"}
         control={control}
